7883d2f550a526ec9ace37b86b24d928dd3e3761
RomanMokeychuk <117006475+RomanMokeychuk@users.noreply.github.com>
2023-06-01 18:07:25 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function () {
+    $(".slider-blog__inner").slick({
+        dots: true,
+        prevArrow:
+            '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow:
+            '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    autoplay: true,
+                },
+            },
+        ],
+    });
+
+    $(".menu__btn").on("click", function () {
+        $(".header__top-inner").toggleClass("header__top-inner--active");
+    });
+
+    var mixer = mixitup(".portfolio__content");
+});
